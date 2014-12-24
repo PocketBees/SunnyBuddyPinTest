7f08d743495d0d3de0c3df05d38c6d4dd35552bf
Cody Knutson <cody.knutson@sparkfun.com>
2014-12-24 11:41:04 -0700

    Update SunnyBuddyPinTest.ino
    Nested code into a function for easier code integration.

--- SunnyBuddyPinTest.ino
+++ SunnyBuddyPinTest.ino
@@ -13,25 +13,32 @@ void setup() {
 
 void loop() {
   
+   sunnyStatus(); //sunny buddy status pin function
+  
+}
+
+
+
+
+void sunnyStatus(){ //sunny buddy status pin function
+    
   int chargeState = digitalRead(9); 
   //If the battery is good, fault is high
   //If the battery requires additional charge and has a power source, pin will be low
   if (digitalRead(9) == LOW && digitalRead(8) == HIGH){
-    Serial.println("Battery is charging");
-   }
+      Serial.println("Battery is charging");
+  }
 
   int faultState = digitalRead(8);
-  
   if (faultState == LOW) {
-    Serial.println("Bad Battery"); //Fault should always be high unless battery is bad
+      Serial.println("Bad Battery"); //Fault should always be high unless battery is bad
   }
-  delay(200);
+  //the rest of this function diplays the results over serial
   Serial.print("Charge: ");
   Serial.println(chargeState);
   Serial.print("Fault: ");
   Serial.println(faultState);
   delay(2000);        
 }
-
 
 
